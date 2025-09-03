f5911241a2e9c0766d66f9c684b464e47ed038f9
Lincxx <bossco28@gmail.com>
2025-09-03 08:40:10 -0400

    constants

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
-<?php 
-
-// echo 'Hello World';
-// print 'Hello World' ;
-// echo('Hello World');
-// print('Hello World');
-
-// $name = 'John';
-// echo "Hello $name"; 
-// echo "Hello $name";  -->
-?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -18,12 +7,9 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>
-    <?php echo 'Hello World'; ?>
-    <?= 'Hello World'?>
-    </h1>
-
-    <?php $x = 10; $y = 20;  ?>
-    <p><?php echo $x + $y; ?></p>
+   <h2>Lessons</h2>
+   <ul>
+    <li><a href="PHP-Say-What/008-constants.php">008 Constants</a></li>
+   </ul>
 </body>
 </html>
